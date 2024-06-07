a6f49249ff772c284c95c6867c19314ca126d154
juliamoschen <juliamoschenpessoa@gmail.com>
2024-06-07 19:31:16 -0300

    Update Jogo_Paciencia.hpp

--- Jogo_Paciencia.hpp
+++ Jogo_Paciencia.hpp
@@ -3,13 +3,39 @@
 
 #include "jogo.hpp"
 
-class Jogo_Paciencia : protected jogo
-{
+struct noCarta {
+      int valor;
+      char naipe;
+      struct noCarta *prox;
+};
+
+class Jogo_Paciencia : public Jogo {
 private:
-protected:
+    noCarta *monte;
+    noCarta *pilhaDeBaixo[7];
+    noCarta *pilhaDeCima[4];
+    noCarta *atual;
+    int linhaEscondida[7];
+
+    void criarMonte();
+    noCarta *removeMonte(int);
+    void embaralharMonte();
+    void distribuiCartas();
+    void imprimeCartas();
+    void proximaCarta();
+    int podeMover(noCarta *, noCarta *);
+    int moveBaixoBaixo(int, int, int);
+    int moveMonteBaixo(int);
+    int naipeCima(char);
+    int moveBaixoCima(int);
+    int moveMonteCima();
+    int moveCimaBaixo(char, int);
+
 public:
-  
+    Jogo_Paciencia();
 
-}
+    void explica_jogo() override;
+    void Start() override;
+};
 
- #endif
+#endif
